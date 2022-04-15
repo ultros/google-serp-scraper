69cf6eeea32119639fe8691c8ec1f6e203e6815d
Jesse Shelley <jesse@cybertutorials.org>
2022-04-14 21:53:03 -0600

    Updated FQDN to show all sub-folders.

--- main.py
+++ main.py
@@ -6,22 +6,20 @@ import re
 
 def parse_links(your_domain, keyword):
     '''Finds and parses a list of the first Google SERP.'''
-    fqdn_item_split = []
+    full_link = []
     response = requests.get(f"https://www.google.com/search?num=100&q={keyword}")
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     links = soup.find_all('a')
 
     for link in links:
-        parsed_link = re.findall('\A<a href="/url\?.*?;', str(link))
+        if link:
+            parsed_link = re.findall('\A<a href="/url\?.*?"><', str(link))
+            for link in parsed_link:
+                sliced_link = link[24:-3]
+                split_link = sliced_link.split('&amp')
+                full_link.append(split_link[0])
 
-        if parsed_link:
-            for parsed_url in parsed_link:
-                if not re.search("google|youtube", str(parsed_url)):
-                    fqdn = f"{parsed_url[24:-1]}"
-                    fqdn_item = fqdn.split("/")
-                    fqdn_item_split.append(fqdn_item[0])
-
-    return fqdn_item_split
+    return full_link
 
 
 def parse_sitelinks(fqdn_item_split):
@@ -40,12 +38,14 @@ def get_rank(parsed_fqdn, your_domain, keyword):
     '''Gets current pagerank of domain on Google Search Engine Results Page.'''
     i = 1
     for item in parsed_fqdn:
-        if item == your_domain:
-            print(f"{i}: {item} - You rank {i}  for \"{item}\" with keyword: {keyword}")
+        link = item.split('/')
+        if link[0] == your_domain:
+            print(f"!!! {i}: {item}\n!!! You rank {i} for \"{item}\" with keyword: \"{keyword}\"")
         else:
-            print(f"{i}: {item}")
+            print(f"{i}: {item}  ")
 
         i = i + 1
+    return
 
 
 if __name__=="__main__":
